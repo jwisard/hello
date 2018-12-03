9565493f16ee73a7821bb390ec1f6eef2d6207ab
Jeff Wisard <jwisard@cas.org>
2018-12-03 10:01:09 -0500

    added stringutil.Reverse

--- hello.go
+++ hello.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "github.com/jwisard/stringutil"
+)
 
 func main() {
-  fmt.Println("Hello, world")
+  fmt.Println(stringutil.Reverse("!oG ,olleH"))
 }
